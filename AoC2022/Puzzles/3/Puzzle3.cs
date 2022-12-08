606acc5c161e52519a822dd9fd79e0925380b33b
remsus <remco.van.es7@hotmail.com>
2022-12-08 17:23:59 +0100

    add 3 and 4 (probably broken will run it later)

---
+++ AoC2022/Puzzles/3/Puzzle3.cs
@@ -0,0 +1,82 @@
+﻿using AoC2022.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2022.Puzzles._3
+{
+    public class Puzzle3
+    {
+        private string[] input;
+        public Puzzle3() 
+        {
+            input = FileReader.GetInputForDay(3);
+        }    
+        public void SolvePartOne()
+        {
+            string result = "";
+            foreach(string s in input)
+            {
+                result += GetMatchingCharacter(s);
+            }
+            var score = calculateScoreForAll(result);
+
+            Console.WriteLine(score);
+        }
+
+        public void SolvePartTwo() 
+        {
+            string result = "";
+            for(int i = 0; i < input.Length; i+=3 )
+            {
+                result += GetMatchingCharacterForThreeGroups(input[i], input[i+1], input[i+2]);
+            }
+            var score = calculateScoreForAll(result);
+
+            Console.WriteLine(score);
+        }
+
+        private char GetMatchingCharacter(string input)
+        {
+            string one = input.Substring(input.Length / 2);
+            string two = input.Substring(0, input.Length / 2);
+
+            foreach(char c in one)
+            {
+                if (two.Contains(c))
+                {
+                    return c;
+                }
+            }
+
+            return 'a';
+        }
+
+        private char GetMatchingCharacterForThreeGroups(string one, string two, string three)
+        {
+            foreach (char c in one)
+            {
+                if (two.Contains(c) && three.Contains(c))
+                {
+                    return c;
+                }
+            }
+
+            return 'a';
+        }
+
+        private int calculateScoreForAll(string input)
+        {
+            int total = 0;
+            foreach(char c in input)
+            {
+                total += CharacterToValue.GetValueForCharacter(c);
+            }
+            return total;
+        }
+        
+    }
+}
---
+++ AoC2022/Puzzles/4/Puzzle4.cs
@@ -0,0 +1,63 @@
+﻿using AoC2022.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2022.Puzzles._4
+{
+    public class Puzzle4
+    {
+        string[] input;
+        public Puzzle4()
+        {
+            input = FileReader.GetInputForDay(4);
+        }
+        public void SolvePartOne()
+        {
+            int total = 0;
+            for(int i = 0; i < input.Length; i++) 
+            {
+                if (DoesOneContainTheOther(input[i].Split(',')[0], input[i].Split(',')[1]))
+                    total++;
+            }
+            Console.WriteLine(total);
+        }
+
+        public void SolvePartTwo() 
+        {
+            int total = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (HasOverlap(input[i].Split(',')[0], input[i].Split(',')[1]))
+                    total++;
+            }
+            Console.WriteLine(total);
+        }
+
+        private bool DoesOneContainTheOther(string one, string two)
+        {
+            int onesmall = int.Parse(one.Split('-')[0]);
+            int onebig = int.Parse(one.Split('-')[1]);
+
+            int twosmall = int.Parse(two.Split('-')[0]);
+            int twobig = int.Parse(two.Split('-')[1]);
+
+            return ((onesmall >= twosmall && onebig <= twobig) || (onesmall <= twosmall && onebig >= twobig));
+
+        }
+
+        private bool HasOverlap(string one, string two)
+        {
+            int onesmall = int.Parse(one.Split('-')[0]);
+            int onebig = int.Parse(one.Split('-')[1]);
+
+            int twosmall = int.Parse(two.Split('-')[0]);
+            int twobig = int.Parse(two.Split('-')[1]);
+
+            return ((onesmall <= twobig && onebig >= twosmall) || (twosmall <= onebig && twobig >= onesmall));
+
+        }
+    }
+}
